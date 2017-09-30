8304657b32573ec2b66d535d80301bed0d704bdb
Michael Hunger <github@jexp.de>
2017-09-30 19:51:57 +0200

    add allow-uri-read flag to render

--- render.sh
+++ render.sh
@@ -3,5 +3,4 @@ file=${file%%.txt}
 file=${file%%.adoc}
 file=${file##adoc/}
 echo $file
-asciidoctor $1 -a img=../img -D html/
-open -a Camino html/$file.html
+asciidoctor $1 -a allow-uri-read -a img=../img -D html/
